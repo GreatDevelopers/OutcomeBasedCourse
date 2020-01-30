a5e04a9bf4eb4af4280c27977ffc8a70e04ae963
SurajDadral <dadralj18@gmail.com>
2020-01-31 00:36:18 +0530

    Implement outcomes as list in course form

--- course/forms.py
+++ course/forms.py
@@ -716,7 +716,32 @@ class UnitForm(forms.Form):
         super(UnitForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "POST"
-        self.helper.add_input(Submit("submit", "Submit"))
+        self.helper.form_class = "form-horizontal"
+        self.helper.label_class = "col-md-3"
+        self.helper.field_class = "col-md-9"
+        self.helper.layout = Layout(
+            Div(
+                Field("unit_name"),
+                HTML("<br>"),
+                Field("unit_short_name"),
+                HTML("<br>"),
+                Field("unit_overview"),
+                HTML("<br>"),
+                Fieldset("Add Outcomes", FormsetLayoutObject("outcomes")),
+                HTML("<br>"),
+                Field("unit_objective"),
+                HTML("<br>"),
+                Field("unit_body"),
+                HTML("<br>"),
+                Field("unit_resources"),
+                HTML("<br>"),
+                Field("unit_test"),
+                HTML("<br>"),
+                Field("module"),
+                HTML("<br>"),
+                ButtonHolder(Submit("submit", "Submit")),
+            )
+        )
 
 
 OutcomeFormSet = forms.models.modelformset_factory(
